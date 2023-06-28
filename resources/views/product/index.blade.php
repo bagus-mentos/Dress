e5d6e22602cb2b96938604a7841825f3e67a5fec
Fahmi Nur Rahman <fachminur@gmail.com>
2023-06-28 22:23:43 +0700

    product image preview onclick

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -45,6 +45,17 @@
         </div>
     </div>
 
+    <div class="modal fade" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <img src="" class="imagepreview" style="width: 100%;">
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 @endsection
 @push('scripts')
@@ -92,25 +103,25 @@
                 , {
                     data: "pic1"
                     , render: function(data) {
-                       return "<img src=\"products/" + data + "\" height=\"50\"/>";
+                        return "<a href='#' class='pop'><img src=\"products/" + data + "\" height=\"50\"/></a>";
                     }
-                },
-                {
+                }
+                , {
                     data: "pic2"
                     , render: function(data) {
-                       return "<img src=\"products/" + data + "\" height=\"50\"/>";
+                        return "<a href='#' class='pop'><img src=\"products/" + data + "\" height=\"50\"/></a>";
                     }
-                },
-                {
+                }
+                , {
                     data: "pic3"
                     , render: function(data) {
-                       return "<img src=\"products/" + data + "\" height=\"50\"/>";
+                        return "<a href='#' class='pop'><img src=\"products/" + data + "\" height=\"50\"/></a>";
                     }
-                },
-                {
+                }
+                , {
                     data: "pic4"
                     , render: function(data) {
-                       return "<img src=\"products/" + data + "\" height=\"50\"/>";
+                        return "<a href='#' class='pop'><img src=\"products/" + data + "\" height=\"50\"/></a>";
                     }
                 }
                 , {
@@ -120,6 +131,11 @@
                     , searchable: false
                 }
             , ]
+        });
+
+        $("body").on("click", ".pop", function(e) {
+            $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+            $('#imagemodal').modal('show');
         });
     });
 
